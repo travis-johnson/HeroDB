128f47c4c9aa8ce4b84ae9c5e91fb97ef988a5ed
Travis Johnson <trav.johnson919@gmail.com>
2017-10-14 19:02:13 -0400

    initial commit

--- script/script.js
+++ script/script.js
@@ -1,14 +1,12 @@
 var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "http://comicvine.gamespot.com/api/characters/?api_key=ee0dbaffd32c89c6a7b8e9745670e0fbe379e3c0&filter=name%3Apunisher&field_list=description",
-    "method": "GET",
-    "headers": {
-        "cache-control": "no-cache",
-        "postman-token": "ec191fdd-0eb4-cb15-3e89-3d265ea53930"
-    }
+    "url": "http://comicvine.gamespot.com/api/characters/?api_key=ee0dbaffd32c89c6a7b8e9745670e0fbe379e3c0&filter=name:punisher&field_list=description&format=json",
+    "method": "GET"
+
 }
 
 $.ajax(settings).done(function(response) {
-    console.log(response);
+    $("#content").html(response.results[0].description);
+    console.log(response.results.description);
 });
