0ff62276232d63081965e0c692c02eefce3f1afa
travis-johnson <trav.johnson919@gmail.com>
2017-10-17 15:30:10 -0400

    Basic styling started, also trying to tie in the input value for search capability.

--- script/script.js
+++ script/script.js
@@ -1,12 +1,15 @@
+var x = $("#search").val();
+
 var settings = {
-    "async": true,
-    "crossDomain": true,
+    "async": false,
     "url": "http://comicvine.gamespot.com/api/characters/?api_key=ee0dbaffd32c89c6a7b8e9745670e0fbe379e3c0&filter=name:punisher&field_list=description&format=json",
     "method": "GET"
 
 }
-
 $.ajax(settings).done(function(response) {
     $("#content").html(response.results[0].description);
+
+
     console.log(response.results.description);
+    console.log(search)
 });
