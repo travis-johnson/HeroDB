c8c34ee03a0c73bc3fa5d7014170b39f0ae3f056
Travis Johnson <trav.johnson919@gmail.com>
2017-10-25 23:31:22 -0400

    Form is able to search and clear results on each new search.  Currently working on styling search results.

--- script/script.js
+++ script/script.js
@@ -3,35 +3,20 @@ $(document).ready(function() {
         e.preventDefault();
         var search = $('#search').val();
         var settings = {
-            "async": true,
-            "url": "http://comicvine.gamespot.com/api/characters/?api_key=ee0dbaffd32c89c6a7b8e9745670e0fbe379e3c0&filter=name:" + search + "&field_list=description&format=json",
-            "method": "GET"
+                "async": true,
+                "url": "http://comicvine.gamespot.com/api/characters/?api_key=ee0dbaffd32c89c6a7b8e9745670e0fbe379e3c0&filter=name:" + search + "&field_list=description&format=json",
+                "method": "GET"
 
-        }
+            }
+            //This empties out the content of the div upon each new search
+        $("#content").empty();
         $.ajax(settings).done(function(response) {
             var ct = $("#content");
             for (i = 0; i < response.results.length; i++) {
                 ct.append(response.results[i].description);
-
-
-                console.log(response.results[i].description);
-                console.log(search);
             }
         });
 
     });
 
 });
-
-// $("form").submit(function() {
-//     var x = $("form");
-//     $.ajax({
-//         url: "http://comicvine.gamespot.com/api/characters/?api_key=ee0dbaffd32c89c6a7b8e9745670e0fbe379e3c0&filter=name:cyclops&field_list=description",
-//         dataType: "html",
-//         success: function(response) {
-//             $('#content').html(response.results[0]);
-//             console.log(response.results[0]);
-//         }
-//     });
-
-// });
