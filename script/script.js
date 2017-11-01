f63bfff848bdd6f3c0db326d95f3a8e263491adc
Travis Johnson <trav.johnson919@gmail.com>
2017-11-01 16:53:43 -0400

    trying a workaround the the ACAO

--- script/script.js
+++ script/script.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
                 "url": "https://comicvine.gamespot.com/api/characters/?api_key=ee0dbaffd32c89c6a7b8e9745670e0fbe379e3c0&filter=name:" + search + "&format=json",
                 "method": "GET",
                 "contentType": "text/plain",
-                "Access-Control-Allow-Origin": "https://comicvine.gamespot.com"
+                "Access-Control-Allow-Origin": "*"
 
             }
             //This empties out the content of the div upon each new search
