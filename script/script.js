2c32d945fc423864b989b6ef8037035a91c473ae
travis-johnson <trav.johnson919@gmail.com>
2017-11-03 00:46:15 -0400

    added bootstrap classes to images and adjusted font-size for h3 elements.

--- script/script.js
+++ script/script.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
 
 
 
-            $("img").addClass("img-fluid");
+            $("img").addClass("img-fluid rounded float-left");
             $('a').each(function() {
                 this.href = this.href.replace('file:///F:', 'https://comicvine.gamespot.com')
                 this.href = this.href.replace('https://travis-johnson.github.io', 'https://comicvine.gamespot.com')
